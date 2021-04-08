0a6865604d540ec757c20243d9c8ec554ae12b07
ManiChandana46 <70904491+ManiChandana46@users.noreply.github.com>
2021-04-08 21:05:11 +0530

    Update GroceryyStoreApplication.java

--- GroceryyStore/src/main/java/com/lti/GroceryyStoreApplication.java
+++ GroceryyStore/src/main/java/com/lti/GroceryyStoreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GroceryyStoreApplication extends springbootservletinitializer {
+public class GroceryyStoreApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GroceryyStoreApplication.class, args);
